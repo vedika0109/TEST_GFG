0de0f6edefecbc6a63cc9ee9498994d1573a1efb
Vedika Choudhary <118730174+vedika0109@users.noreply.github.com>
2025-06-28 20:04:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Counting elements in two arrays/counting-elements-in-two-arrays.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    vector<int> countLessEq(vector<int>& a, vector<int>& b) {
+        // code here
+         sort(b.begin(), b.end());
+        vector<int> ans;
+        for (int x : a) {
+            int k = upper_bound(b.begin(), b.end(), x) - b.begin();
+            ans.push_back(k);
+        }
+        return ans;
+    }
+};
